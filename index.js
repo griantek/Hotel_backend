a578f075326b1d4cba1310ec3ceec5f9ab5f23fb
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2025-01-15 21:18:07 +0530

    backend_mesaege

--- index.js
+++ index.js
@@ -350,7 +350,7 @@ app.get('/api/bookings/:id', (req, res) => {
   );
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT =  4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
