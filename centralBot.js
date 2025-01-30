03bc9e3e0f1b30b63f8bf9d12486f7aa92cd52cf
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2025-01-30 22:02:30 +0530

    added admin and customer bot separate

--- centralBot.js
+++ centralBot.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 
 // Import bot handlers
-const adminBot = require('./adminBot');
-const customerBot = require('./customerBot');
+const adminBot = require('./adminBot.js');
+const customerBot = require('./customerBot.js');
 
 // Webhook verification endpoint
 app.get('/spa', (req, res) => {
